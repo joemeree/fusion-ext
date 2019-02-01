d85d514415e6e4d994169c7bfa5996240f34fec6
Joe Meree <jmeree@gmail.com>
2019-02-01 17:21:33 -0500

    add scripts to "get" local and aws web1

---
+++ getPut/bin/getWeb1Aws.sh
@@ -0,0 +1,2 @@
+bash getApp.sh --app web1 --dir /data/downloads/git/fusiongsa --password searchr0cks --server 54.70.30.221 --debug
+
---
+++ getPut/bin/getWeb1Local.sh
@@ -0,0 +1,2 @@
+getApp.sh --app web1 --dir /data/downloads/git/fusiongsa --password searchr0cks
+
